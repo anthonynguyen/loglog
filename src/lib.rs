d3a165010ddd77634be66d40622c736f7f08cf02
Anthony Nguyen <anknguyen@gmail.com>
2017-08-15 12:07:10 -0400

    Add coloured level

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate ansi_term;
 extern crate env_logger;
 #[macro_use] extern crate error_chain;
 extern crate log;
@@ -6,13 +7,22 @@ mod errors;
 pub use errors::*;
 
 pub fn init() -> Result<()> {
-	env_logger::LogBuilder::new()
-		.format(formatter).filter(None, log::LogLevelFilter::Info)
-		.init()?;
+    env_logger::LogBuilder::new()
+        .format(formatter)
+        .filter(None, log::LogLevelFilter::Trace)
+        .init()?;
 
-	Ok(())
+    Ok(())
 }
 
 fn formatter(record: &log::LogRecord) -> String {
-	format!("{} - {}", record.level(), record.args())
+    let level = match record.level() {
+        log::LogLevel::Trace => ansi_term::Colour::Blue.paint("[TRACE]"),
+        log::LogLevel::Debug => ansi_term::Colour::Cyan.paint("[DEBUG]"),
+        log::LogLevel::Info => ansi_term::Colour::Green.paint("[INFO] "),
+        log::LogLevel::Warn => ansi_term::Colour::Yellow.paint("[WARN] "),
+        log::LogLevel::Error => ansi_term::Colour::Red.paint("[ERROR]"),
+    };
+
+    format!("{} {} - {}", level, record.target(), record.args())
 }
